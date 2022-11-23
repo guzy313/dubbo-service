b9f828e017bd5ea477b6710a48cf60890117e4da
顾哲宇 <guzy@yeyoo.cn>
2022-11-23 18:35:35 +0800

    dubbo-service first commit

---
+++ src/main/java/com/my/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.my.service.impl;
+
+import com.my.service.UserService;
+
+/**
+ * @author Gzy
+ * @version 1.0
+ * @Description
+ */
+public class UserServiceImpl  implements UserService{
+
+
+    @Override
+    public String hihi() {
+        return "HIHI";
+    }
+}
