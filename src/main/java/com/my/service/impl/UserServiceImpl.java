04e8421aad7cad92676368e48b8d1b8bcfc819ca
顾哲宇 <guzy@yeyoo.cn>
2022-11-24 12:04:47 +0800

    更改配置文件

--- src/main/java/com/my/service/impl/UserServiceImpl.java
+++ src/main/java/com/my/service/impl/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.my.service.impl;
 
 import com.my.service.UserService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Gzy
  * @version 1.0
  * @Description
  */
+@Service
 public class UserServiceImpl  implements UserService{
 
 
